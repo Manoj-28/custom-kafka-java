93ef89ef239244a9046372babda99068ff82007c
Manoj-28 <manojbhoye9928@gmail.com>
2024-10-26 09:19:43 +0530

    socket connection

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,30 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
+    public static void main(String[] args){
+        System.err.println("Logs from your program will appear here!");
+
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
+        int port = 9092;
+        try {
+            serverSocket = new ServerSocket(port);
+            // REUSEADDR ensures that we don't run into 'Address already in use' errors
+            serverSocket.setReuseAddress(true);
+            // Wait for connection from client.
+            clientSocket = serverSocket.accept();
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        } finally {
+            try {
+                if (clientSocket != null) {
+                    clientSocket.close();
+                }
+            } catch (IOException e) {
+                System.out.println("IOException: " + e.getMessage());
+            }
+        }
+    }
 }
