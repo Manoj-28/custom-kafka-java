10aa26aca7ee439b57674f60901db2ba4ac8ec32
Manoj-28 <manojbhoye9928@gmail.com>
2024-10-26 10:38:49 +0530

    Parse Correlation ID

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -16,9 +17,21 @@ public class Main {
             serverSocket.setReuseAddress(true);
             // Wait for connection from client.
             clientSocket = serverSocket.accept();
+
+            //echo -n "00000023001200046f7fc66100096b61666b612d636c69000a6b61666b612d636c6904302e3100" | xxd -r -p | nc localhost 9092 | hexdump -C
+            InputStream in = clientSocket.getInputStream();
             OutputStream out = clientSocket.getOutputStream();
+
+            byte[] messageSize = new byte[4];
+            in.read(messageSize);
+
+            in.skip(4);
+
+            byte[] correlationIdBytes = new byte[4];
+            in.read(correlationIdBytes);
+
             out.write(new byte[] {0,0,0,0});   //Message size
-            out.write(new byte[] {0,0,0,7});   // Correlation ID
+            out.write(correlationIdBytes);   // Correlation ID
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         } finally {
